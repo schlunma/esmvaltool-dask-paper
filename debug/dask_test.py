b51296ccef5af8d6b8ed079d5880add7b64d2e50
Manuel Schlund <manuel.schlund@dlr.de>
2024-11-20 17:45:23 +0100

    Update debug recipe

--- debug/dask_test.py
+++ debug/dask_test.py
@@ -1,47 +1,94 @@
 from dask.distributed import Client
 import dask.array as da
+import xarray as xr
+import numpy as np
+from pathlib import Path
 
 
-client = Client('tcp://127.0.0.1:40365')
-print("Using client", client)
-print()
+# Real data
+paths = [
+    '/work/bd0854/DATA/ESMValTool2/CMIP6_DKRZ/HighResMIP/MIROC/NICAM16-9S/highresSST-present/r1i1p1f1/day/tas/gr/v20190830/tas_day_NICAM16-9S_highresSST-present_r1i1p1f1_gr_19500101-19501231.nc',
+    '/work/bd0854/DATA/ESMValTool2/CMIP6_DKRZ/HighResMIP/MIROC/NICAM16-9S/highresSST-present/r1i1p1f1/day/tas/gr/v20190830/tas_day_NICAM16-9S_highresSST-present_r1i1p1f1_gr_19510101-19511231.nc',
+    '/work/bd0854/DATA/ESMValTool2/CMIP6_DKRZ/HighResMIP/MIROC/NICAM16-9S/highresSST-present/r1i1p1f1/day/tas/gr/v20190830/tas_day_NICAM16-9S_highresSST-present_r1i1p1f1_gr_19520101-19521231.nc',
+    '/work/bd0854/DATA/ESMValTool2/CMIP6_DKRZ/HighResMIP/MIROC/NICAM16-9S/highresSST-present/r1i1p1f1/day/tas/gr/v20190830/tas_day_NICAM16-9S_highresSST-present_r1i1p1f1_gr_19530101-19531231.nc',
+    '/work/bd0854/DATA/ESMValTool2/CMIP6_DKRZ/HighResMIP/MIROC/NICAM16-9S/highresSST-present/r1i1p1f1/day/tas/gr/v20190830/tas_day_NICAM16-9S_highresSST-present_r1i1p1f1_gr_19540101-19541231.nc',
+]
 
-array = da.ones((3000, 1280, 2560), chunks=("auto", -1, -1))
-weights_2d = da.ones((1280, 2560))
+# Dummy data
+small_shape = (365, 1280, 2560)
+big_shape = (366, 1280, 2560)
+chunks = (10, -1, -1)
+out_dir = Path("/scratch/b/b309141/tmp")
+nc_paths = [
+    out_dir / "1.nc",
+    out_dir / "2.nc",
+    out_dir / "3.nc",
+    out_dir / "4.nc",
+    out_dir / "5.nc",
+]
+zarr_paths = [
+    out_dir / "1.zarr",
+    out_dir / "2.zarr",
+    out_dir / "3.zarr",
+    out_dir / "4.zarr",
+    out_dir / "5.zarr",
+]
 
-weights_3d = da.broadcast_to(weights_2d, shape=array.shape, chunks=array.chunks)
+# print("Saving nc")
+# for path in nc_paths:
+#     if "2" in str(path):
+#         shape = big_shape
+#     else:
+#         shape = small_shape
+#     arr = xr.DataArray(
+#         da.random.random(shape, chunks=chunks),
+#         name="x",
+#         dims=("time", "lat", "lon"),
+#     )
+#     arr.to_netcdf(path)
+#     print("Saved", path)
+# print()
 
-avg = da.average(array, axis=(1, 2), weights=weights_3d)
+# print("Saving zarr")
+# for path in zarr_paths:
+#     if "2" in str(path):
+#         shape = big_shape
+#     else:
+#         shape = small_shape
+#     arr = da.random.random(shape, chunks=chunks)
+#     da.to_zarr(arr, path)
+#     print("Saved", path)
+# print()
 
-print("array", array)
-print("weights_2d", weights_2d)
-print("weights_3d", weights_3d)
+
+# Actual test code
+# client = Client('tcp://127.0.0.1:35073')
+# print("Using client", client)
+# print()
+
+
+# good
+# arrs = [
+#     da.random.random(big_shape, chunks=chunks) if "2" in str(p) else
+#     da.random.random(small_shape, chunks=chunks) for p in zarr_paths
+# ]
+
+# bad
+# arrs = [da.from_zarr(p) for p in zarr_paths]
+
+# good
+# dss = [xr.open_dataset(p, chunks={"time": 10, "lat": -1, "lon": -1}) for p in nc_paths]
+# arrs = [ds.x.data for ds in dss]
+
+# bad
+dss = [xr.open_dataset(p, chunks={"time": 10, "lat": -1, "lon": -1}) for p in paths]
+arrs = [ds.tas.data for ds in dss]
+
+
+arr = da.concatenate(arrs, axis=0)
+weights = da.ones_like(arr, chunks=arr.chunks)
+avg = da.sum(arr * weights, axis=(1, 2)) / da.sum(weights, axis=(1, 2))
+print("arr", arr)
+print("weights", weights)
 print("avg", avg)
-
 print(avg.compute())
-
-
-
-# arr = da.ones((1280, 2560))
-# broad_arr = 
-
-# n = 11
-# small_shape = (365, 1280, 2560)
-# large_shape = (n * small_shape[0], *small_shape[1:])
-# chunks = (10, 1280, 2560)
-# # chunks = (-1, "auto", "auto")
-
-# # x = da.ones(large_shape, chunks=chunks)
-
-# arrs = [da.ones(small_shape, chunks=chunks) for _ in range(n)]
-# # arrs = [da.ones(small_shape) for _ in range(n)]
-# x = da.concatenate(arrs, axis=0)
-
-# w = da.ones(x.shape, chunks=x.chunksize)
-# print("x", x, x.chunks)
-# print("w", w, w.chunks)
-
-# y = da.average(x, axis=(1, 2), weights=w)
-# print("y", y, y.chunks)
-
-# print(y.compute())
